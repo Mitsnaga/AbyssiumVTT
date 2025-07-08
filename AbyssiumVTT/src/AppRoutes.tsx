9777b7480bac117a724f193da5d71524d146a88c
MACS <macs270500@gmail.com>
2025-07-08 22:55:07 +0400

    Fix React Router and ensure routing is working with Outlet and RouterProvider. Navbar button added and tested

--- AbyssiumVTT/src/AppRoutes.tsx
+++ AbyssiumVTT/src/AppRoutes.tsx
@@ -1,12 +1,14 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import { Charsheet } from './pages/charsheet';
 import App from './App';
+import LandingPage from "./pages/landingpage";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
+            { index: true, element:<LandingPage />},
             { path: 'charsheet', element: <Charsheet /> },
         ]
     }
